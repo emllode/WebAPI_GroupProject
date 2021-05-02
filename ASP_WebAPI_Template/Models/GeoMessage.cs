268a085fd42c366cd7b29064a7b7fc9f709284f8
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-05-02 18:26:02 +0200

    Model for geoMessage, done.

---
+++ ASP_WebAPI_Template/Models/GeoMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_WebAPI_Template.Models
+{
+    public class GeoMessage
+    {
+
+        public string Message { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}
